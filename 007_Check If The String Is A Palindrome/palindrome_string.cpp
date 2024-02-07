23db9e962cd52ae80e3e038d67d49ecc4e92a6ce
Aditya Mondal <130544041+CodingWizard07@users.noreply.github.com>
2024-02-07 23:01:05 +0530

    Create palindrome_string.cpp

---
+++ 007_Check If The String Is A Palindrome/palindrome_string.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+#include <cctype>
+#include <string>
+
+char to_lower(char ch){
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        return ch - 'A' + 'a';
+    }
+    return ch;
+}
+string remove(const string &s)
+{
+    string r;
+    for (char ch : s)
+    {
+        if (isalnum(ch))
+        {
+            r += to_lower(ch);
+        }
+    }
+    return r;
+}
+
+bool checkPalindrome(string s)
+{
+    string result = remove(s);
+
+    int start = 0;
+    int end = result.length() - 1;
+
+    while (start <= end)
+    {
+        if (to_lower(result[start]) != to_lower(result[end]))
+        {
+            return false;
+        }
+        else
+        {
+            start++;
+            end--;
+        }
+    }
+    return true;
+}
